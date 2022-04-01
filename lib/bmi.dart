da6944e9255c52ececfae90789ea57d397c06f2a
Putu Bagus Angga Darmawan <bagusangga2301@gmail.com>
2022-04-01 21:11:43 +0800

    .

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -8,6 +8,9 @@ class InputBMI extends StatefulWidget {
 }
 
 class _InputBMIState extends State<InputBMI> {
+  final name = TextEditingController();
+  final jenis = TextEditingController();
+  final umur = TextEditingController();
   int tinggi = 0;
   int berat = 0;
 
@@ -37,12 +40,65 @@ class _InputBMIState extends State<InputBMI> {
                 ),
               ),
 
+              const SizedBox(height: 10),
+              TextFormField(
+                //memberikan identitas untuk setiap form
+                controller: name,
+                decoration: InputDecoration(
+                    hintText: "Ketik nama lengkap ",
+                    labelText: "Masukan Nama lengkap",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                //memberikan validasi jika form kosong
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: jenis,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                    labelText: 'Jenis kelamin',
+                    hintText: 'Masukan jenis kelamin ',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'jenis kelamin tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: umur,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                    labelText: 'Umur',
+                    hintText: 'Masukan Umur Anda ',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Umur tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+
               Container(
                   margin: EdgeInsets.only(top: 20),
                   padding: EdgeInsets.all(10),
                   // color: Colors.blue[700],
                   child: Row(
                     children: <Widget>[
+
                       Expanded(
                         child: TextField(
                           onChanged: (txt) {
@@ -95,7 +151,8 @@ class _InputBMIState extends State<InputBMI> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => BMIResult(tinggi_badan: tinggi, berat_badan: berat)),
+                      MaterialPageRoute(builder: (context) => BMIResult( name: name.text,
+                          jenis: jenis.text,umur: umur.text, tinggi_badan: tinggi, berat_badan: berat)),
                     );
                   },
                   padding: EdgeInsets.only(top: 10, bottom: 10),
@@ -121,7 +178,7 @@ class _InputBMIState extends State<InputBMI> {
             color: Colors.black54,
             alignment: Alignment.center,
             child: Text(
-              'Powerred by Putu Bagus Angga Darmawan',
+              'Putu Bagus Angga Darmawan Nim : 1915101048',
               style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w500,
--- lib/bmi_result.dart
+++ lib/bmi_result.dart
@@ -4,7 +4,11 @@ import 'dart:math';
 
 class BMIResult extends StatelessWidget {
 
-  BMIResult({required this.tinggi_badan, required this.berat_badan});
+  BMIResult({required this.name,
+    required this.jenis,required this.umur,required this.tinggi_badan, required this.berat_badan});
+  final String name;
+  final String jenis;
+  final String umur;
   final int tinggi_badan;
   final int berat_badan;
 
@@ -27,6 +31,18 @@ class BMIResult extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Text(
+              name,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              jenis,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              umur,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
             Text(
               cBMI,
               style: TextStyle(
